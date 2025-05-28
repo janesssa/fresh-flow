ded1d75af91a082ec5f262401542ab2a362d060b
Janessa <janessa@thuisgekookt.nl>
2025-05-28 16:45:14 +0200

    added nest and postgres

---
+++ mealprep-backend/src/recipes/recipes.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { RecipesService } from './recipes.service';
+import { RecipesController } from './recipes.controller';
+import { Recipe } from './recipe.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RecipesController } from './recipes.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Recipe])],
+  providers: [RecipesService],
+  controllers: [RecipesController],
+})
+export class RecipesModule {}
